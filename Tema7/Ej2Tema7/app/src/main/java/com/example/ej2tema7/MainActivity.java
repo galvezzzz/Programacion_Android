9b4fee382320d6a29b5b204792eacdfb59d9d6cc
Alberto <bichi1609@gmail.com>
2025-02-12 09:23:28 +0100

    Ejercicio 2 Tema 7 - Clase

--- Tema7/Ej2Tema7/app/src/main/java/com/example/ej2tema7/MainActivity.java
+++ Tema7/Ej2Tema7/app/src/main/java/com/example/ej2tema7/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ej2tema7;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,16 +21,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences pref = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
 
         Button btnDefinir = findViewById(R.id.btnDefinir);
-
-
+        Button btnRecuperar = findViewById(R.id.btnRecuperar);
 
         btnDefinir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Opciones.class);
                 startActivity(intent);
+            }
+        });
+
+        btnRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                Log.i("","Opción 1: " + pref.getBoolean("opcion1",false));
+                Log.i("","Opción 2: " + pref.getString("opcion2",""));
+                Log.i("", "Opción 3:" + pref.getString("opcion3",""));
+
             }
         });
     }
